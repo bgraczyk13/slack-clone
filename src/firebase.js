d6201f7490340214b8011ec18f5a74850c888e62
bgraczyk <robgrac94@gmail.com>
2021-11-10 15:58:13 -0500

    navbar commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAB3YXkRdYOFmoo0kxxQYDud9PtDGwFCmM",
+  authDomain: "slack-clone-62d87.firebaseapp.com",
+  projectId: "slack-clone-62d87",
+  storageBucket: "slack-clone-62d87.appspot.com",
+  messagingSenderId: "1021161502776",
+  appId: "1:1021161502776:web:30bdf07f0d77ed65570ecd",
+};
